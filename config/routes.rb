db7a1cfc45fc5f8578853c9ed5d9a73e3125b3c1
CyberRacc <89751580+CyberRacc@users.noreply.github.com>
2024-05-27 13:54:05 +0100

    chore: Add initial project files and configuration

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "articles#index"
+
+  get '/articles', to: 'articles#index'
+
+  # The `:id` denotes a route parameter. It is a placeholder for the actual ID of the article.
+  # A route parameter caputres a segment of the request's path, and puts that value into the `params` hash.
+  # When handling a request like `GET http://localhost:3000/articles/1`, `1` would be captures as the value for `:id`
+  # which would then be accessible as `params[:id]` in the show action of the ArticlesController.
+  get '/articles/:id', to: 'articles#show'
+end
