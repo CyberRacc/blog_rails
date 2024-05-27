377b227515203143adb47dfe5a0dab2a799c1612
CyberRacc <89751580+CyberRacc@users.noreply.github.com>
2024-05-27 16:19:48 +0100

    feat: Add nested resource for comments in articles
    This commit adds a nested resource for comments in the articles routes. By nesting comments within articles, it enforces that comments can only be created, read, updated, or deleted in the context of an article. This improves the organization and structure of the application's routes.

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,15 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "articles#index"
 
-  get '/articles', to: 'articles#index'
+  resources :articles do
+    # Comments are a nested resource within articles.
+    # This means that a comment can only be created, read, updated, or deleted in the context of an article.
+    resources :comments
+  end
 
   # The `:id` denotes a route parameter. It is a placeholder for the actual ID of the article.
   # A route parameter caputres a segment of the request's path, and puts that value into the `params` hash.
   # When handling a request like `GET http://localhost:3000/articles/1`, `1` would be captures as the value for `:id`
   # which would then be accessible as `params[:id]` in the show action of the ArticlesController.
-  get '/articles/:id', to: 'articles#show'
+  # get '/articles/:id', to: 'articles#show'
 end
